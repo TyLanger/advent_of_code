0fbb422db7b9e9f6d5f074ed32a001cb6c6efb49
TyLanger <ty.langer16@gmail.com>
2022-12-05 01:15:48 -0600

    Day 05 Clippy
    Used clippy to make it look nicer.
    Add and remove some comments.
    
    input is awkward
    Some fns, I pass the whole input, some I trim it before. In the ones I
    pretrim, I don't even do a good job of pretrimming and need to trim a bit
    more before looping over the lines.
    I should fix that somehow.
    Either pass the whole input into each fn or trim once into the 3?
    different forms I need with the lines properly trimmed.

--- src/bin/day_05.rs
+++ src/bin/day_05.rs
@@ -13,7 +13,7 @@ fn part_1(input: &str) -> String {
     // get the top of each stack
     // return the tops.
 
-    let num_stacks = get_number_of_stacks(&input);
+    let num_stacks = get_number_of_stacks(input);
     let mut stacks = Vec::new();
     for _ in 0..num_stacks {
         let stack: Vec<String> = Vec::new();
@@ -24,10 +24,11 @@ fn part_1(input: &str) -> String {
     let start_stack_input: Vec<&str> = input.split('1').collect();
 
     setup_stacks(&mut stacks, start_stack_input[0]);
-    // println!("{:?}", stacks);
 
     let instructions = input.split_once(&num_stacks.to_string());
-    // split at the end of stack labels
+    // split at the end of stack labels (3 or 9)
+    // it actually doesn't matter. Could've been 1 bc I am skipping
+    // that line anyway
 
     process_stacks(&mut stacks, instructions.unwrap().1);
 
@@ -35,13 +36,12 @@ fn part_1(input: &str) -> String {
 }
 
 fn part_2(input: &str) -> String {
-
     // get initial state
     // calculate all the moves
     // get the top of each stack
     // return the tops.
 
-    let num_stacks = get_number_of_stacks(&input);
+    let num_stacks = get_number_of_stacks(input);
     let mut stacks = Vec::new();
     for _ in 0..num_stacks {
         let stack: Vec<String> = Vec::new();
@@ -52,15 +52,14 @@ fn part_2(input: &str) -> String {
     let start_stack_input: Vec<&str> = input.split('1').collect();
 
     setup_stacks(&mut stacks, start_stack_input[0]);
-    // println!("{:?}", stacks);
 
     let instructions = input.split_once(&num_stacks.to_string());
     // split at the end of stack labels
 
+    // only part that is different from part 1
     process_stacks_part_2(&mut stacks, instructions.unwrap().1);
 
     get_top_of_each_stack(stacks)
-
 }
 
 fn get_number_of_stacks(input: &str) -> u32 {
@@ -78,51 +77,41 @@ fn get_number_of_stacks(input: &str) -> u32 {
     all_nums.last().unwrap().parse().unwrap()
 }
 
-fn setup_stacks(stacks: &mut Vec<Vec<String>>, input: &str) {
+fn setup_stacks(stacks: &mut [Vec<String>], input: &str) {
     // need to start at the bottom
     let lines = input.lines();
     // split at 1 in the line below.
     // skip the line that is the few spaces in front of the 1
     for line in lines.rev().skip(1) {
-        // let crates = line.split('[');
-        // _, Z] ,M] ,P] 
-        // len should be 4
-        // _, N] ,C] 
-        // len is 3 for 2 crates
-        // _, A]     ,C]
-        // for 2 crates with a gap in the middle
-        // 5 spaces, len 3
-        // ____,D]
-        // 4 spaces for a gap
-
         // input has consistent spacing
         // check if there is a letter at position 1, 5, 9, etc.
         // if it exists, push it into that stack
-        // let line_as_bytes = line.as_bytes();
         let individual_chars: Vec<&str> = line.split("").collect();
         let mut letter_offset = 2;
-        for i in 0..stacks.len() {
+        for stack in stacks.iter_mut() {
             if letter_offset >= individual_chars.len() {
                 break;
             }
             // check if a letter exists at the offset
-            if !individual_chars[letter_offset].contains(" ") {
-                stacks[i].push(individual_chars[letter_offset].to_string());
+            if !individual_chars[letter_offset].contains(' ') {
+                stack.push(individual_chars[letter_offset].to_string());
             }
             letter_offset += 4;
         }
     }
 }
 
-fn process_stacks(stacks: &mut Vec<Vec<String>>, input: &str) {
-
+fn process_stacks(stacks: &mut [Vec<String>], input: &str) {
+    // _
     // \n\r\n\r
     // move 1 from 2 to 1
     // move 3 from 1 to 3
     // ..
 
     let lines = input.lines();
-    // skip the blank line
+    // skip the blank lines
+    // the bit after the 3 or 9
+    // and the gap line between the crates and instructions
     for line in lines.skip(2) {
         let inst = Instruction::from_string(line);
 
@@ -133,8 +122,8 @@ fn process_stacks(stacks: &mut Vec<Vec<String>>, input: &str) {
     }
 }
 
-fn process_stacks_part_2(stacks: &mut Vec<Vec<String>>, input: &str) {
-
+fn process_stacks_part_2(stacks: &mut [Vec<String>], input: &str) {
+    // _
     // \n\r\n\r
     // move 1 from 2 to 1
     // move 3 from 1 to 3
@@ -145,7 +134,7 @@ fn process_stacks_part_2(stacks: &mut Vec<Vec<String>>, input: &str) {
     // put into a temp stack
 
     let lines = input.lines();
-    // skip the blank line
+    // skip the blank lines
     for line in lines.skip(2) {
         let inst = Instruction::from_string(line);
 
@@ -186,7 +175,7 @@ struct Instruction {
 impl Instruction {
     fn from_string(line: &str) -> Self {
         let input: Vec<&str> = line.split_whitespace().collect();
-        
+
         let amount = input[1].parse().unwrap();
         let start = input[3].parse().unwrap();
         let dest = input[5].parse().unwrap();
