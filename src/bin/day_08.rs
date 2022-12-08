44f2a896bfdf106f77e395404e32dc1b79c5f8f7
TyLanger <ty.langer16@gmail.com>
2022-12-08 02:03:12 -0600

    Day 08 Part 2
    Rank 11722, 15779
    Took a break between parts. Might've been my best placing.
    
    I got so mixed up with the coordinates for some reason. Maybe I should've
    made som helper fns like get_up() and then they would've been easier to
    test and find my issues.
    
    clippy
    Learned I can do .count() instead of .collect() and .len(). I was trying
    .len() so I didn't find .count()

--- src/bin/day_08.rs
+++ src/bin/day_08.rs
@@ -4,7 +4,7 @@ fn main() {
     // remember to change the input
     let file = fs::read_to_string("./inputs/day_08_input.txt").unwrap();
     println!("{}", part_1(&file));
-    // println!("{}", part_2(&file));
+    println!("{}", part_2(&file));
 }
 
 fn part_1(input: &str) -> usize {
@@ -24,7 +24,7 @@ fn part_1(input: &str) -> usize {
     let mut trees = create_tree_vec(tree_heights);
 
     // trees[12].visible = true;
-    
+
     calculate_visibility(&mut trees, width, height);
 
     // dbg!(&trees);
@@ -32,22 +32,25 @@ fn part_1(input: &str) -> usize {
 }
 
 fn part_2(input: &str) -> u32 {
-    todo!("part 2")
+    let width = get_width(input);
+    let height = get_height(input);
+
+    let tree_heights = get_height_vec(input);
+    let trees = create_tree_vec(tree_heights);
+
+    find_best_scenic_score(trees, width, height)
 }
 
 fn get_width(input: &str) -> usize {
-
-    for line in input.lines() {
-        let split: Vec<&str> = line.split("").filter(|x| !x.is_empty()).collect();
-        return split.len();
+    if let Some(line) = input.lines().next() {
+        // for line in input.lines() {
+        return line.split("").filter(|x| !x.is_empty()).count();
     }
     0
 }
 
 fn get_height(input: &str) -> usize {
-
-    let lines: Vec<&str> = input.lines().collect();
-    lines.len()
+    input.lines().count()
 }
 
 fn get_height_vec(input: &str) -> Vec<u32> {
@@ -56,10 +59,7 @@ fn get_height_vec(input: &str) -> Vec<u32> {
     for line in input.lines() {
         // line_count += 1;
         // dbg!(line_count);
-        let mut split: Vec<u32> = line
-            .split("")
-            .filter_map(|x| x.parse().ok())
-            .collect();
+        let mut split: Vec<u32> = line.split("").filter_map(|x| x.parse().ok()).collect();
         num_v.append(&mut split);
     }
 
@@ -77,20 +77,18 @@ fn create_tree_vec(tree_heights: Vec<u32>) -> Vec<Tree> {
 }
 
 fn get_num_visible_trees(trees: Vec<Tree>) -> usize {
-    let v: Vec<&Tree> = trees.iter().filter(|x| x.visible).collect();
-    v.len()
+    trees.iter().filter(|x| x.visible).count()
 }
 
-fn calculate_visibility(trees: &mut Vec<Tree>, width: usize, height: usize) {
+fn calculate_visibility(trees: &mut [Tree], width: usize, height: usize) {
     // left to right
     for i in 0..width {
         let mut largest = 0;
         for j in 0..height {
-
             let index = i * width + j;
 
             //edges
-            if i == 0 || i == (width-1) || j==0 || j==(width-1) {
+            if i == 0 || i == (width - 1) || j == 0 || j == (width - 1) {
                 trees[index].visible = true;
             }
 
@@ -105,7 +103,6 @@ fn calculate_visibility(trees: &mut Vec<Tree>, width: usize, height: usize) {
     for i in (0..width).rev() {
         let mut largest = 0;
         for j in (0..height).rev() {
-
             let index = i * width + j;
 
             if trees[index].height > largest {
@@ -119,7 +116,6 @@ fn calculate_visibility(trees: &mut Vec<Tree>, width: usize, height: usize) {
     for j in 0..height {
         let mut largest = 0;
         for i in 0..width {
-
             let index = i * width + j;
 
             if trees[index].height > largest {
@@ -133,7 +129,6 @@ fn calculate_visibility(trees: &mut Vec<Tree>, width: usize, height: usize) {
     for j in (0..height).rev() {
         let mut largest = 0;
         for i in (0..width).rev() {
-
             let index = i * width + j;
 
             if trees[index].height > largest {
@@ -142,6 +137,93 @@ fn calculate_visibility(trees: &mut Vec<Tree>, width: usize, height: usize) {
             }
         }
     }
+}
+
+fn find_best_scenic_score(trees: Vec<Tree>, width: usize, height: usize) -> u32 {
+    let mut best_score = 0;
+
+    // index is left to right
+
+    for i in 1..(width - 1) {
+        for j in 1..(height - 1) {
+            let index = i * width + j;
+            // println!("index: {}", index);
+            let my_height = trees[index].height;
+
+            let mut up_view = 1;
+            let mut right_view = 1;
+            let mut down_view = 1;
+            let mut left_view = 1;
+
+            // check up
+            for u in 1..i {
+                let u_index = (i - u) * width + j;
+                // println!("u_index: {}", u_index);
+                if u_index < trees.len() {
+                    if trees[u_index].height < my_height {
+                        up_view += 1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            // check down
+            for u in 1..(height - i - 1) {
+                let d_index = (i + u) * width + j;
+                // println!("d_index: {}", d_index);
+                if d_index < trees.len() {
+                    if trees[d_index].height < my_height {
+                        down_view += 1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            // check right
+            for u in 1..(width - j - 1) {
+                let r_index = index + u;
+                // println!("r_index: {}", r_index);
+                if r_index < trees.len() {
+                    if trees[r_index].height < my_height {
+                        right_view += 1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            // check left
+            for u in 1..j {
+                let l_index = index - u;
+                // println!("l_index: {}", l_index);
+                if l_index < trees.len() {
+                    if trees[l_index].height < my_height {
+                        left_view += 1;
+                    } else {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            let score = up_view * right_view * down_view * left_view;
+            // println!("({}, {}) = {}  {} {} {} {}", i, j, score, up_view, right_view, down_view, left_view);
+            if score > best_score {
+                best_score = score;
+            }
+        }
+    }
+
+    best_score
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -179,11 +261,10 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "not ready"]
     fn part_2_works() {
         let result = part_2(&DAY_8_BASIC_INPUT);
 
-        assert_eq!(21, result);
+        assert_eq!(8, result);
     }
 
     #[test]
