1a12ba398a0f2b3dae4d0fb8b92be98008c79d2a
TyLanger <ty.langer16@gmail.com>
2022-12-17 02:53:54 -0600

    Day 17 Part 2 Start
    Doesn't work
    I tried to find when a full row is filled. When this happens, I can
    deleted everything below it. Reset the values of stuff above the line to
    small numbers so the heigh number never gets too big.
    Except that isn't really the issue.
    The issue is a trillion is too big. I can't simulate that much. a million
    takes 25s. A trillion would take like 200 days.
    
    This new culling version doesn't even work on part 1. it's close, but
    probably off by 1 somewhere that I can't find.
    
    Back to the drawing board trying to figure out how to find when it
    repeats.

--- src/bin/day_17.rs
+++ src/bin/day_17.rs
@@ -1,12 +1,10 @@
-use std::{
-    collections::{BTreeSet, HashSet},
-    fs,
-};
+use std::{collections::HashSet, fs};
 
 fn main() {
     let input = fs::read_to_string("./inputs/day_17_input.txt").unwrap();
 
-    println!("{}", part_1(&input));
+    // println!("{}", part_1(&input)); // 3232
+    println!("{}", part_2(&input));
 }
 
 fn part_1(input: &str) -> i32 {
@@ -41,6 +39,8 @@ fn part_1(input: &str) -> i32 {
             _ => {}
         }
     }
+    // 40 movements
+    // println!("movement length: {:?}", movements.len());
 
     let mut peak_height = -1;
     let mut settled_rocks = 0;
@@ -110,13 +110,214 @@ fn part_1(input: &str) -> i32 {
     }
 
     // println!("obstacles: {:?}", &obstacles);
-    // draw_obstacles(&obstacles, peak_height);
+    draw_obstacles(&obstacles, peak_height);
 
-    peak_height + 1
+    peak_height
 }
 
 fn part_2(input: &str) -> i32 {
-    99
+    // probably need to figure out when this repeats
+    // there are 40 movements
+    // 5 blocks
+    // each block will use x movements
+
+    // maybe whenever I get floor, I can set that to the new floor and restart?
+
+    let angle_brackets: Vec<&str> = input.split("").collect();
+    let mut movements = Vec::new();
+    for item in angle_brackets {
+        match item {
+            "<" => {
+                movements.push(Movement::Left);
+            }
+            ">" => {
+                movements.push(Movement::Right);
+            }
+            _ => {}
+        }
+    }
+    // 40 movements
+    // println!("movement length: {:?}", movements.len());
+
+    let mut peak_height = -1;
+    let mut settled_rocks = 0;
+
+    let mut rock_index = 0;
+    let mut movement_index = 0;
+
+    let mut obstacles = HashSet::new();
+
+    // let mut inner_count = 0;
+
+    let count: u64 = 1_000_000_000_000;
+    let height: u64 = 1514285714288;
+
+    let mut heights = Vec::new();
+
+    // takes ~25s
+    let million = 1_000_000;
+    // 1B would take 7 hours
+    // 1T would take 7000 hours = 289 days
+
+    while settled_rocks < 2022 {
+        // println!("Settled rocks: {}", settled_rocks);
+        let mut block = Block::from_rock(get_rock(rock_index), peak_height);
+        rock_index += 1;
+
+        loop {
+            // println!("block can fall");
+            // println!("block: {:?}", block);
+
+            match movements[movement_index] {
+                Movement::Left => {
+                    if block.can_move_left(&obstacles) {
+                        block.move_left();
+                        // println!("move left");
+                    } else {
+                        // hit a wall
+                        // don't need to do anything I don't think
+                    }
+                }
+                Movement::Right => {
+                    if block.can_move_right(&obstacles) {
+                        block.move_right();
+                        // println!("move right");
+                    } else {
+                    }
+                }
+            }
+            // wrap around
+            movement_index = (movement_index + 1) % movements.len();
+
+            if block.can_move_down(&obstacles) {
+                block.move_down();
+                // println!("move down");
+            } else {
+                // println!("stop rock");
+                // add block to obstacles
+                settled_rocks += 1;
+                let height = block.get_peak_height();
+                if height > peak_height {
+                    peak_height = height;
+                }
+                // peak_height = block.get_peak_height();
+                // println!("Block stopped. height: {:?}", peak_height);
+                for p in block.points.iter() {
+                    obstacles.insert(*p);
+                }
+
+                // was a line made??
+                // if yes, where?
+                let new_line = line_logic(&mut obstacles, height);
+                if let Some(new_line) = new_line {
+                    // let line_diff = peak_height - new_line;
+
+                    // draw old
+                    // draw_obstacles(&obstacles, peak_height);
+
+                    heights.push(peak_height);
+
+                    cull_obstacles(&mut obstacles, new_line, peak_height);
+
+                    // draw new
+                    // println!("New obstacles");
+                    // peak_height = 0;
+                    peak_height -= new_line;
+                    // println!("new peak: {}", peak_height);
+                    // draw_obstacles(&obstacles, peak_height);
+                    // if heights.len() == 2 {
+                    //     println!("heights: {:?}", heights);
+                    //     return 99;
+                    // }
+                    // return 99;
+                }
+
+                break;
+            }
+
+            // inner_count += 1;
+            // if inner_count > 100 {
+            // return 99;
+
+            // }
+        }
+    }
+
+    // println!("obstacles: {:?}", &obstacles);
+    draw_obstacles(&obstacles, peak_height);
+
+    // len 59
+    // len 19
+    // len 21
+    println!("heights: {:?} len: {}", heights, heights.len());
+    let mut sum: u64 = 0;
+    for h in heights {
+        sum += h as u64;
+    }
+
+    // should be 3068 for 2022 blocks
+    // 3413
+    // off by 345
+    // 3024
+    // off by 44
+    // 3083
+    // off by 15
+
+    // I was running, not testing
+    // test input repeats, but doesn't make lines
+
+    // should be 3232 for real input
+    // 3083 + 118, len 21
+    // off by 149
+    // adding peak and len is 3222. Off by 10
+    // does that make sense?
+
+    // 3122 + 98. len 35
+    // 3190 + 179. len 23
+    // 3034 + 156. len 28
+
+    println!("Calculated height: {}", sum);
+    peak_height
+}
+
+fn line_logic(obstacles: &mut HashSet<Point>, last_placed_height: i32) -> Option<i32> {
+    // only loop over 5 nearby heights
+    // last placed height is the block's heaight,
+    // not the greatest height
+    // lines are probably made below the height
+    'height: for y in ((last_placed_height - 5)..=last_placed_height).rev() {
+        let mut count = 0;
+        for x in 0..7 {
+            let p = Point::new(x, y);
+            if obstacles.contains(&p) {
+                count += 1;
+            } else {
+                continue 'height;
+            }
+        }
+        if count == 7 {
+            // full line found
+            return Some(y);
+        }
+    }
+    None
+}
+
+fn cull_obstacles(obstacles: &mut HashSet<Point>, new_line: i32, max_height: i32) {
+    let mut new_obstacles = HashSet::new();
+    // skip the real line
+    // or you get double hits
+    // the next block will match on this line that got reinserted
+    for y in (new_line + 1)..=max_height {
+        for x in 0..7 {
+            let p = Point::new(x, y);
+            if obstacles.contains(&p) {
+                let new_p = Point::new(x, p.y - (new_line + 1));
+                new_obstacles.insert(new_p);
+            }
+        }
+    }
+    *obstacles = new_obstacles;
 }
 
 fn draw_obstacles(obstacles: &HashSet<Point>, height: i32) {
@@ -347,6 +548,7 @@ mod tests {
     const BASIC_INPUT_DAY_17: &str = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 
     #[test]
+    #[ignore = "done for now"]
     fn part_1_works() {
         assert_eq!(3068, part_1(&BASIC_INPUT_DAY_17));
     }
