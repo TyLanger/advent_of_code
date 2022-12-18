21b118a3aad94a48d6d5e3e7485795695b5ffa1a
TyLanger <ty.langer16@gmail.com>
2022-12-17 19:41:15 -0600

    Day 17 Part 2 Complete
    Rank 3049, 9033
    part 1 done at ~1am. Figure out part 2 at 4am? then waited til 7pm to
    actually implement it. So part 2 could've been better.
    
    Need to run for 1T rocks
    
    What ended up working was scrolling through the output for repeats. I
    found a repeat at height 275 that matched height 3034. From there, I can
    simulate to figure out which block made that line. It was block 171 and
    1911.
    Every 1740 rocks, this repeats. The tower gets 1740 rocks and 2759 height.
    Extrapolate for 1T
    
    A couple mistakes using line height where I was supposed to use
    rocks_settled and some off by 1.
    
    Only works for my specific input.
    I find the matching spots by hand.
    Would be tricky to make it work automatically.
    I don't think the repeat extrapolates down to height 0. The starting is a
    bit unique I think.
    
    only the last ~5 lines of part 2 are actually needed. It's just doing the
    math using the heights and stuff I found by hand.

--- src/bin/day_17.rs
+++ src/bin/day_17.rs
@@ -116,6 +116,288 @@ fn part_1(input: &str) -> i32 {
 }
 
 fn part_2(input: &str) -> i32 {
+    // does it repeat after 200 iterations?
+    // 40 inputs, 5 blocks
+    // simulate 200
+    // get the height.
+    // figure out the overlap
+
+    // 1T / 200 = x
+    // height * x is the answer?
+
+    let angle_brackets: Vec<&str> = input.split("").collect();
+    let mut movements = Vec::new();
+    for item in angle_brackets {
+        match item {
+            "<" => {
+                movements.push(Movement::Left);
+            }
+            ">" => {
+                movements.push(Movement::Right);
+            }
+            _ => {}
+        }
+    }
+    // 40 movements
+    // 10091 for real input
+    println!("movement length: {:?}", movements.len());
+
+    let mut peak_height = -1;
+    let mut settled_rocks = 0;
+
+    let mut rock_index = 0;
+    let mut movement_index = 0;
+
+    let mut obstacles = HashSet::new();
+
+    // let mut inner_count = 0;
+
+    while settled_rocks < 1180 {
+        // println!("Settled rocks: {}", settled_rocks);
+        let mut block = Block::from_rock(get_rock(rock_index), peak_height);
+        rock_index += 1;
+
+        loop {
+            // println!("block can fall");
+            // println!("block: {:?}", block);
+
+            match movements[movement_index] {
+                Movement::Left => {
+                    if block.can_move_left(&obstacles) {
+                        block.move_left();
+                        // println!("move left");
+                    } else {
+                        // hit a wall
+                        // don't need to do anything I don't think
+                    }
+                }
+                Movement::Right => {
+                    if block.can_move_right(&obstacles) {
+                        block.move_right();
+                        // println!("move right");
+                    } else {
+                    }
+                }
+            }
+            // wrap around
+            movement_index = (movement_index + 1) % movements.len();
+            // if movement_index == 0 && rock_index % 5 == 0 {
+            //     println!("movement at 0. count: {} rock_index mod: {} height: {}", settled_rocks, rock_index, peak_height);
+            // }
+            // when these match, height goes up by ~27
+            // 22 + 28 + 27 + 26 + 27 + 26 + 27 + 26 + 27 + 26 + 27
+            // movement at 0. count: 14 rock_index mod: 15 height: 22
+            // +15 +15 +28
+            // movement at 0. count: 29 rock_index mod: 30 height: 50
+            // +20 +20 +27
+            // movement at 0. count: 49 rock_index mod: 50 height: 77
+            // +15 +15 +26
+            // movement at 0. count: 64 rock_index mod: 65 height: 103
+            // +20 +20 +27
+            // movement at 0. count: 84 rock_index mod: 85 height: 130
+            // +15 +15 +26
+            // movement at 0. count: 99 rock_index mod: 100 height: 156
+            // +20 +20 +27
+            // movement at 0. count: 119 rock_index mod: 120 height: 183
+            // +15 +15 +26
+            // movement at 0. count: 134 rock_index mod: 135 height: 209
+            // +20 +20 +27
+            // movement at 0. count: 154 rock_index mod: 155 height: 236
+            // +15 +15 +26
+            // movement at 0. count: 169 rock_index mod: 170 height: 262
+            // +20 +20 +27
+            // movement at 0. count: 189 rock_index mod: 190 height: 289
+            // every 15 settled rocks, +26 height
+            // every 20 settled rocks, +27 height
+            // every 35 rocks, + 53 height
+            // after the first 29 rocks and 50 height
+
+            // 2022 - 35 = 1987
+            // 1987 / 45 = 44.1555
+            // 45 * 53 = 2385
+            // 50 + 2288 = 2338
+            // 50 + 2385 = 2435
+            // real 3068
+            // off by 633
+
+            // 2022 - 29 = 1993
+            // 1993 / 35 = 56.94
+            // 56 * 53 = 2968
+            // off by 100
+            // 35 * 56 = 1960
+            // 1960 + 29 = 1989
+            // 33 to evaluate at the end
+            // can account for +15 -> +26
+            // or +20 -> 27
+            // 50 + 2968 + 26 + 27 = 3071
+            // off by 3
+
+            // pattern keeps going to 2024 as expected
+            // why is it +15 +20?
+            // 15 isn't a factor of 40 or 200
+            // 15 * 20 = 300
+
+            // Part 2
+            // Pattern
+            // no movement == 0 && rock == 0
+            // in 2m iterations
+
+            if block.can_move_down(&obstacles) {
+                block.move_down();
+                // println!("move down");
+            } else {
+                // println!("stop rock");
+                // add block to obstacles
+                settled_rocks += 1;
+                let height = block.get_peak_height();
+                if height > peak_height {
+                    peak_height = height;
+                }
+                // peak_height = block.get_peak_height();
+                // println!("Block stopped. height: {:?}", peak_height);
+                for p in block.points.iter() {
+                    obstacles.insert(*p);
+                }
+
+                let new_line = line_logic(&mut obstacles, height);
+                if let Some(new_line) = new_line {
+                    if new_line == 275 {
+                        println!("line 275 filled at rocks: {}", settled_rocks);
+                        draw_obstacles_window(&obstacles, new_line);
+                    } else if new_line == 292 {
+                        println!("line 292 filled at rocks: {}", settled_rocks);
+                        draw_obstacles_window(&obstacles, new_line);
+                    } else if new_line == 3034 {
+                        println!("line 3034 filled at rocks: {}", settled_rocks);
+                        draw_obstacles_window(&obstacles, new_line);
+                    } else if new_line == 5793 {
+                        println!("line 5793 filled at rocks: {}", settled_rocks);
+                        draw_obstacles_window(&obstacles, new_line);
+                    }
+                }
+
+                break;
+            }
+
+            // inner_count += 1;
+            // if inner_count > 100 {
+            // return 99;
+
+            // }
+        }
+    }
+
+    // println!("obstacles: {:?}", &obstacles);
+    // draw_obstacles(&obstacles, peak_height);
+
+    // 307
+
+    // bottom
+    // |#####..|
+    // |..###..|
+    // |...#...|
+    // |..####.|
+    // top
+    // |.......|
+    // |.##....|
+    // |.##...#|
+    // |..#...#|
+    // |..#.###|
+    // |..#..#.|
+    // |..#.###|
+    // |.#####.|
+    // |....#..|
+
+    // should stack nicely
+    // 2022 / 200 = 10.11
+    // 307 * 10.11 = 3103.77
+    // test == 3068
+    // diff of 35
+
+    // println!("Height after 200: {}", peak_height);
+
+    // line 3040 matches line 281
+    // 3055 matches 296
+    // diff 2759
+    // 292 is a tetris
+    // figure out when that happens?
+
+    // I need to map 1T rocks to a height
+    // I don't know how many iterations gave me line 281
+
+    // after 171 rocks, line 275 is filled (height 275)
+    // after 209 rocks, line 292 is filled. Worse candidate.
+    // It's filled by a line piece falling down a crack
+
+    // 275 -> 171
+    // 3040-6 = 3034
+    // 3034 -> ?
+
+    // 3034 -> 1911
+    // 5793 -> 3651
+
+    // rocks
+    // 1911 - 171 = 1740
+    // should repeat again after another 1740 blocks
+    // 1911 + 1740 = 3651 rocks
+    // height should be: 3034 + 2759 = 5793
+
+    // expect:
+    // line 5793 is a line
+    // its rock count is 3651
+
+    // what to do?
+    // keep adding lines until it breaches 1T
+    let base_rock_count: u64 = 171;
+    let pattern_rock_count: u64 = 1740;
+    // at 171 + 1740 = 1911
+    // 1911 + 1740 = 3651
+    // 3651 + 1740 = 5391
+
+    let height_growth: u64 = 2759;
+
+    // reverse
+    // 5391 - 171 = 5220
+    // 5220 / 1740 = 3
+    // 6000 - 171 = 5829
+    // 5829 / 1740 = 3.35
+    // leftover = 5829 % 1740 = 609
+    // then I need the height after 171 + 609 iterations?
+    // height = 171 + repeats * 2759 + h(609?)
+    let big = 1_000_000_000_000 - base_rock_count;
+    let repeats = big / pattern_rock_count;
+    println!("repeats: {}", repeats);
+    let remainder = big % pattern_rock_count;
+    println!("remainder: {}", remainder);
+
+    // 1T repeats: 574_712_643
+    // 1T remainder: 1009
+    let naive_height: u64 = repeats * height_growth;
+    // println!("naive height: {}", naive_height);
+    // 1T naive: 1_585_632_182_037
+
+    // after 2 repeats, 2 * 2759 = 5518
+    // 5518 + 275 = 5793
+
+    // total_height = repeats * growth + height at 275+remainder?
+    // height after 275+1009 = 1284
+    // h(1284) = 2032 +- 1?
+    // 171 + 1009 = 1180
+    // h(1180) = 1877
+
+    // 1585632184069 too high
+    // -1 still too high
+    // 1585632183914 is too low
+    // 1585632183915 is correct
+    println!(
+        "Height after 1T: {}",
+        naive_height + (peak_height + 1) as u64
+    );
+
+    peak_height
+}
+
+fn part_2_broken(input: &str) -> i32 {
     // probably need to figure out when this repeats
     // there are 40 movements
     // 5 blocks
@@ -331,7 +613,30 @@ fn draw_obstacles(obstacles: &HashSet<Point>, height: i32) {
                 line = format!("{}{}", line, ".");
             }
         }
-        println!("{}|", line);
+        if y % 5 == 0 {
+            println!("{}| {}", line, y);
+        } else {
+            println!("{}|", line);
+        }
+    }
+}
+
+fn draw_obstacles_window(obstacles: &HashSet<Point>, height: i32) {
+    for y in ((height - 10)..=(height + 5)).rev() {
+        let mut line = "|".to_string();
+        for x in 0..7 {
+            let p = Point::new(x, y);
+            if obstacles.contains(&p) {
+                line = format!("{}{}", line, "#");
+            } else {
+                line = format!("{}{}", line, ".");
+            }
+        }
+        if y % 5 == 0 {
+            println!("{}| {}", line, y);
+        } else {
+            println!("{}|", line);
+        }
     }
 }
 
@@ -555,7 +860,7 @@ mod tests {
 
     #[test]
     fn part_2_works() {
-        // 1514285714288
+        // 1_514_285_714_288
         assert_eq!(0, part_2(&BASIC_INPUT_DAY_17));
     }
 
