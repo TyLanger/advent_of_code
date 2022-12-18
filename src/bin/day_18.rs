c8b9125dc55606d4551c67377e2e260022ce75d4
TyLanger <ty.langer16@gmail.com>
2022-12-18 00:53:55 -0600

    Day 18 Part 2
    Rank 3200, 3091
    Best I've done. And done in <2hrs
    
    3D floodfill
    easier to implement than I thought it was going to be.
    wikipedia told me how to use a queue instead of making it recursive which
    was nice of them.
    
    bit hacky finding the outer faces
    test input is 1..3. real input is 0..21
    so I figured I could use Cube x: u32
    but when I did the air, it made air blocks all around the test input, but
    the real input didn't have air blocks at the -1 positions.
    Maybe makign Cube {x: i32} would have been a better solution to get the
    air placement consistent.
    I made neighbours return a vec on none if the neighbour would be out of
    bounds.
    
    The real input has the rocks block off part of the bigger cube.
    So if you flood fill from 0,0,0, it will block off some stuff that is
    accessable to air if you start at 22,22,22 or something.
    So the hack is to have the air extend out of bounds in the positive x,y,z
    directions so it can get to everything.
    It would fail if there were a 'cave' along the x,y,or z face in the
    negative direction. Luckily there wasn't bc I just thought of that now and
    I don't know if I would've figured that out if the test failed.

--- src/bin/day_18.rs
+++ src/bin/day_18.rs
@@ -1,16 +1,16 @@
-use std::{fs, collections::{hash_map, HashSet}};
+use std::{collections::HashSet, fs};
 
 fn main() {
     let input = fs::read_to_string("./inputs/day_18_input.txt").unwrap();
 
-    println!("{}", part_1(&input));
+    println!("{}", part_1(&input)); // 4302
+    println!("{}", part_2(&input)); // 2492
 }
 
 fn part_1(input: &str) -> u32 {
     // 2800 cubes
 
     let cubes = parse_to_cubes(input);
-    // println!("cubes len: {:?}", &cubes.len());
 
     let mut cube_map = HashSet::new();
     for c in &cubes {
@@ -21,20 +21,20 @@ fn part_1(input: &str) -> u32 {
     for c in &cubes {
         let mut hit_count = 0;
 
-        for n in c.get_neighbours() {
-            if cube_map.contains(&n) {
-                hit_count += 1;
+        for n in c.get_neighbours(22, 22, 22) {
+            if let Some(n) = n {
+                if cube_map.contains(&n) {
+                    hit_count += 1;
+                }
             }
         }
         open_faces += 6 - hit_count;
     }
 
-
     open_faces
 }
 
 fn part_2(input: &str) -> u32 {
-
     // get outer faces
     // ignore interior faces
 
@@ -48,8 +48,134 @@ fn part_2(input: &str) -> u32 {
 
     // 3D floodfill
     // i don't know how
+    // start somewhere. then check neighbours. And their neighbours, etc.
 
-    99
+    let cubes = parse_to_cubes(input);
+    // println!("cubes len: {:?}", &cubes.len());
+
+    let mut min_x = 1000;
+    let mut min_y = 1000;
+    let mut min_z = 1000;
+    let mut max_x = 0;
+    let mut max_y = 0;
+    let mut max_z = 0;
+
+    let mut cube_set: HashSet<&Cube> = HashSet::new();
+    for c in &cubes {
+        if c.x < min_x {
+            min_x = c.x;
+        } else if c.x > max_x {
+            max_x = c.x;
+        }
+        if c.y < min_y {
+            min_y = c.y;
+        } else if c.y > max_y {
+            max_y = c.y;
+        }
+        if c.z < min_z {
+            min_z = c.z;
+        } else if c.z > max_z {
+            max_z = c.z;
+        }
+
+        cube_set.insert(c);
+    }
+
+    // 1 3 1 3 1 6
+    // 0 21 0 21 0 20
+    println!(
+        "x: {} {}, y: {} {}, z: {} {}",
+        min_x, max_x, min_y, max_y, min_z, max_z
+    );
+
+    // total cubes = 21 * 21 * 20
+    // solid: cubes.len
+    // air = total - solid
+
+    // If I can find the interior air, I can set them all to cubes
+    // and then the normal algorithm gets the right answer
+
+    // flood fill from 0,0,0 (air)
+    // fill with air
+    // I now have a set of air and a set of blocks
+    // in face count
+    // only count a face if it's air
+    // don't count if another block or nothing
+
+    // flood fill
+    // 0, 0, 0 is air (I checked)
+    let current_cube = Cube { x: 0, y: 0, z: 0 };
+    let mut air: HashSet<Cube> = HashSet::new();
+    air.insert(current_cube);
+
+    let mut queue = Vec::new();
+    queue.push(current_cube);
+
+    while !queue.is_empty() {
+        let current = queue.pop().unwrap();
+        // max + 1
+        // hack
+        // The blocks probably cut you off if you flood fill from 0,0,0
+        // so go out of bounds a bit to get around this block
+        for n in current.get_neighbours(max_x + 1, max_y + 1, max_z + 1) {
+            // is a neighbour in air?
+            // is it a block?
+            if let Some(n) = n {
+                if !within_bounds(&n, max_x + 2, max_y + 2, max_z + 2) {
+                    continue;
+                }
+
+                if cube_set.contains(&n) {
+                    // it's a block
+                    continue;
+                }
+
+                if air.insert(n) {
+                    // was new
+                    queue.push(n);
+                }
+            }
+        }
+    }
+
+    // let num_cubes = cube_set.len();
+    // let num_air = air.len();
+    // let space = (max_x - min_x + 2) * (max_y - min_y + 2) * (max_z - min_z + 2);
+    // println!("num cubes: {:?}", num_cubes);
+    // println!("air set: {:?}", num_air);
+    // println!("cubes + air: {:?}", num_cubes + num_air);
+    // println!("space: {:?}", space);
+    // println!("difference: {:?}", space as usize - (num_cubes + num_air));
+
+    // cubes + air < space
+    // if no cubes, air == 54, space == 54
+    // so floodfill should be correct
+
+    let mut open_faces = 0;
+    for c in &cubes {
+        let mut air_face_count = 0;
+
+        for n in c.get_neighbours(max_x, max_y, max_z) {
+            if let Some(n) = n {
+                if air.contains(&n) {
+                    // air
+                    // good face
+                    air_face_count += 1;
+                }
+            } else {
+                air_face_count += 1;
+            }
+
+            // faces that count:
+            // air
+            // or outside bounds
+        }
+        open_faces += air_face_count;
+    }
+
+    // 2473 is too low
+
+    open_faces
 }
 
 fn parse_to_cubes(input: &str) -> Vec<Cube> {
@@ -66,7 +192,9 @@ fn parse_to_cubes(input: &str) -> Vec<Cube> {
     v
 }
 
-
+fn within_bounds(cube: &Cube, x_max: u32, y_max: u32, z_max: u32) -> bool {
+    cube.x < x_max && cube.y < y_max && cube.z < z_max
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Cube {
@@ -76,30 +204,44 @@ struct Cube {
 }
 
 impl Cube {
-    fn get_neighbours(&self) -> Vec<Cube> {
+    fn get_neighbours(&self, max_x: u32, max_y: u32, max_z: u32) -> Vec<Option<Cube>> {
         let mut v = Vec::new();
 
         //up
-        let up = Cube {
-            x: self.x,
-            y: self.y,
-            z: self.z + 1,
-        };
-        v.push(up);
+        if self.z < max_z {
+            let up = Cube {
+                x: self.x,
+                y: self.y,
+                z: self.z + 1,
+            };
+            v.push(Some(up));
+        } else {
+            v.push(None);
+        }
+
         // right
-        let right = Cube {
-            x: self.x + 1,
-            y: self.y,
-            z: self.z,
-        };
-        v.push(right);
+        if self.x < max_x {
+            let right = Cube {
+                x: self.x + 1,
+                y: self.y,
+                z: self.z,
+            };
+            v.push(Some(right));
+        } else {
+            v.push(None);
+        }
+
         //forward
-        let forward = Cube {
-            x: self.x,
-            y: self.y + 1,
-            z: self.z,
-        };
-        v.push(forward);
+        if self.y < max_y {
+            let forward = Cube {
+                x: self.x,
+                y: self.y + 1,
+                z: self.z,
+            };
+            v.push(Some(forward));
+        } else {
+            v.push(None);
+        }
 
         // down
         if self.z > 0 {
@@ -108,7 +250,9 @@ impl Cube {
                 y: self.y,
                 z: self.z - 1,
             };
-            v.push(down);
+            v.push(Some(down));
+        } else {
+            v.push(None);
         }
 
         // left
@@ -118,7 +262,9 @@ impl Cube {
                 y: self.y,
                 z: self.z,
             };
-            v.push(left);
+            v.push(Some(left));
+        } else {
+            v.push(None);
         }
 
         // back
@@ -128,7 +274,9 @@ impl Cube {
                 y: self.y - 1,
                 z: self.z,
             };
-            v.push(back);
+            v.push(Some(back));
+        } else {
+            v.push(None);
         }
 
         v
@@ -173,29 +321,32 @@ mod tests {
 
     #[test]
     fn test_neighbours() {
-        let cube = Cube {x: 2, y: 2, z: 2};
+        let cube = Cube { x: 2, y: 2, z: 2 };
 
-        let neighbours = cube.get_neighbours();
+        let neighbours = cube.get_neighbours(3, 3, 3);
         assert_eq!(6, neighbours.len());
 
         let expected = vec![
-            Cube {x: 2, y: 2, z: 3},
-            Cube {x: 3, y: 2, z: 2},
-            Cube {x: 2, y: 3, z: 2},
-            Cube {x: 2, y: 2, z: 1},
-            Cube {x: 1, y: 2, z: 2},
-            Cube {x: 2, y: 1, z: 2},
+            Some(Cube { x: 2, y: 2, z: 3 }),
+            Some(Cube { x: 3, y: 2, z: 2 }),
+            Some(Cube { x: 2, y: 3, z: 2 }),
+            Some(Cube { x: 2, y: 2, z: 1 }),
+            Some(Cube { x: 1, y: 2, z: 2 }),
+            Some(Cube { x: 2, y: 1, z: 2 }),
         ];
         assert_eq!(expected, neighbours);
 
-        let cube = Cube {x: 0, y: 0, z: 0};
+        let cube = Cube { x: 0, y: 0, z: 0 };
 
-        let neighbours = cube.get_neighbours();
-        assert_eq!(3, neighbours.len());
+        let neighbours = cube.get_neighbours(3, 3, 3);
+        assert_eq!(6, neighbours.len());
         let expected = vec![
-            Cube {x: 0, y: 0, z: 1},
-            Cube {x: 1, y: 0, z: 0},
-            Cube {x: 0, y: 1, z: 0},
+            Some(Cube { x: 0, y: 0, z: 1 }),
+            Some(Cube { x: 1, y: 0, z: 0 }),
+            Some(Cube { x: 0, y: 1, z: 0 }),
+            None,
+            None,
+            None,
         ];
         assert_eq!(expected, neighbours);
     }
