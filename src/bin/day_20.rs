86e0c7c2b8294d54d1d337c48952a28953e89772
TyLanger <ty.langer16@gmail.com>
2022-12-20 03:18:45 -0600

    Day 20 Part 2
    Rank 4283
    But I cheated
    I found a solution online. Once I read it, I realized this is how I was
    trying to solve the problem with having a list of indicies and a list of
    values.
    This version changes the indices, not the values around. Which makes
    sense.
    And instead of swapping endlessly, it just does +1 and -1 to all the
    indicies that need it.
    
    when moving an element,
    stuff to the right of where you started will all shift left -1
    stuff to the right of where you ended gets shifted back +1
    could just be if start < ix && ix < end => -1, right?
    then set the ending point to what is should be.
    
    rem_euclid
    is basically my add_modd and sub_mod built in. Now I know how to use it.
    
    finding the first, second, third is a bit odd
    I kinda understand it. But haven't looked at it enough to explain it.

--- src/bin/day_20.rs
+++ src/bin/day_20.rs
@@ -1,13 +1,14 @@
-use std::{fs, fmt::Display,};
+use std::{fmt::Display, fs};
 
 fn main() {
     let input = fs::read_to_string("./inputs/day_20_input.txt").unwrap();
 
-    println!("{}", part_1(&input));
+    // println!("{}", part_1(&input));
+    // println!("{}", part_2(&input));
+    println!("{}", part_2_smarter(&input));
 }
 
-fn part_1(input: &str) -> i32 {
-
+fn part_1(input: &str) -> i64 {
     // I want to have the array of numbers
     // and an array of pointers
     // as the numbers move around, the pointers point to where they now are
@@ -35,17 +36,15 @@ fn part_1(input: &str) -> i32 {
 
     let mut values = Vec::new();
 
-
     for (i, line) in input.lines().enumerate() {
-        let num = line.trim().parse::<i32>().unwrap();
-        
+        let num = line.trim().parse::<i64>().unwrap();
+
         let val = Node {
             og_order: i,
             value: num,
         };
 
         values.push(val);
-        
     }
     // println!("Values: {:?}", values);
     // for v in &values {
@@ -53,10 +52,8 @@ fn part_1(input: &str) -> i32 {
     // }
     // println!();
 
-
     let length = values.len();
     for i in 0..length {
-
         // find the node with the right order
         let mut current_index = 0;
         let mut current_value = 0;
@@ -84,11 +81,8 @@ fn part_1(input: &str) -> i32 {
                 values.swap(start, end);
             }
         }
-        
-
-
     }
-    
+
     // for v in &values {
     //     print!("{:}", v.to_string());
     // }
@@ -105,29 +99,163 @@ fn part_1(input: &str) -> i32 {
     let second = values[add_mod(start_index, 2000, length)];
     let third = values[add_mod(start_index, 3000, length)];
 
-    println!("first: {} second: {} third: {}", first.value, second.value, third.value);
+    println!(
+        "first: {} second: {} third: {}",
+        first.value, second.value, third.value
+    );
 
     first.value + second.value + third.value
+}
+
+fn part_2(input: &str) -> i64 {
+    // multiply all values by 811589153
+    let multiplier = 811589153;
+
+    // mix 10 times
+    // part_1() * 10
+
+    // brute force won't work here probably
+
+    let mut values = Vec::new();
+
+    for (i, line) in input.lines().enumerate() {
+        let num = line.trim().parse::<i64>().unwrap();
+
+        let val = Node {
+            og_order: i,
+            value: num * multiplier,
+        };
+
+        values.push(val);
+    }
+
+    let length = values.len();
+    for l in 0..10 {
+        println!("{} loop", l);
+        for i in 0..length {
+            // find the node with the right order
+            let mut current_index = 0;
+            let mut current_value = 0;
+            for (j, node) in values.iter().enumerate() {
+                if node.og_order == i {
+                    current_index = j;
+                    current_value = node.value;
+                    break;
+                }
+            }
+
+            if current_value > 0 {
+                // let current_value = (current_value as usize) % length;
+                for s in 0..current_value {
+                    let s = s as usize;
+                    let start = (current_index + s) % length;
+                    let end = (current_index + s + 1) % length;
+                    values.swap(start, end);
+                }
+            } else {
+                // let abs_value = (current_value.abs() as usize) % length;
+                let abs_value = current_value.abs();
+                for s in 0..abs_value {
+                    let s = s as usize;
+                    let start = sub_mod(current_index, s, length);
+                    let end = sub_mod(current_index, s + 1, length);
+                    values.swap(start, end);
+                }
+            }
+        }
+    }
+
+    let mut start_index = 0;
+    for i in 0..values.len() {
+        if values[i].value == 0 {
+            start_index = i;
+            break;
+        }
+    }
+    let first = values[add_mod(start_index, 1000, length)];
+    let second = values[add_mod(start_index, 2000, length)];
+    let third = values[add_mod(start_index, 3000, length)];
+
+    // first: 1623178306 second: 3246356612 third: 811589153
+    // should be 811589153 2434767459 -1623178306
+    println!(
+        "first: {} second: {} third: {}",
+        first.value, second.value, third.value
+    );
+
+    first.value + second.value + third.value
+}
+
+fn part_2_smarter(input: &str) -> i64 {
+    // https://topaz.github.io/paste/#XQAAAQDOBgAAAAAAAAAFG0sTyga3yMCGEnE4cXAte8gbLEX+YfvmMv85TAjDGfxAWScuAniClINXAsMMCUxnN9KPRregjJe7Y6EIoEi+pnNZbKHQXWtm6NzyxByAEEXDENr7LGl0wWijU8sBgTj66ZwGSrgexQONaZ1rLlwZJYU5P0rGyVn5pyVR5NrKr0SHupMU5t36GvcTFdQNRDFwYPUstaDuCLADIc4rJBmW53NKDFMGHo5prBDIK/m8h9TePMRdHMBl+b2TxnyGOFkIbyYfy0/BOYMM8h6ZHlLNyLH1ZBmq1cFdPGgF0FpYFj0epRnX1cQMv6mHUUhLalZWeTUMfUBBGv5ynYI9VwWOwKDoC30lVZrjjQtLm48CpZjISLt4NKLpMJs4kbDxgrSvcybPcfK8zHkzXAKoDu/nfLRC8/I2Z16bV1uL4O8gRgqQEtzWo2HUstWPwIQyGAtDUHjjbsaitR0CikumHJINapnsxa0I79HVPwugPCPCVXkVBN41WrVolbaU036dUZcFaW58iWvEEKVMNx4MR8o7L2LbW833c1c6kXiEpwX6w1h+JcQvqPfL3F+Cow8bJCYLA2jwtnkWVYxbJaacbYzD3cCPKGPn6fXpytcGp2tIIo1V6OzGo/wBHMYdReJ7QlzQdaVgYxReXzNNyStotDXKFnOxxwHrnFCvxIJuMtj2n56iL78Hqy3seZkrFNjkLoR0GYINgNb7PlRjwMS7jTzObNjI4fSbx1JYOzUUEvsrQwLvHCz7iHHnzEB4+QZgJupA/jXeNrN+rXiIwksGEfbVmRnh/+q6YT0=
+
+    // multiply all values by 811589153
+    let multiplier = 811589153;
+
+    // mix 10 times
+    // part_1() * 10
+
+    let mut values = Vec::new();
+    let mut indicies = Vec::new();
+
+    for (i, line) in input.lines().enumerate() {
+        let num = line.trim().parse::<i64>().unwrap();
+
+        values.push(num * multiplier);
+        indicies.push(i);
+    }
+
+    let length = values.len();
+    for _ in 0..10 {
+        for i in 0..length {
+            let index = indicies[i];
+            let value = values[i];
+            let new_pos = (index as i64 + value).rem_euclid(length as i64 - 1);
+            for ix in indicies.iter_mut() {
+                if *ix > index {
+                    *ix -= 1;
+                }
+                if *ix >= new_pos as usize {
+                    *ix += 1;
+                }
+            }
+            indicies[i] = new_pos as usize;
+        }
+    }
+
+    let ordered_zero = indicies[values.iter().position(|&x| x == 0).unwrap()];
+    let first_index = (ordered_zero + 1000).rem_euclid(length);
+    let second_index = (ordered_zero + 2000).rem_euclid(length);
+    let third_index = (ordered_zero + 3000).rem_euclid(length);
+
+    let mut first = 0;
+    let mut second = 0;
+    let mut third = 0;
+    for i in 0..length {
+        if indicies[i] == first_index {
+            first = values[i];
+        }
+        if indicies[i] == second_index {
+            second = values[i];
+        }
+        if indicies[i] == third_index {
+            third = values[i];
+        }
+    }
+
+    first + second + third
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Node {
     og_order: usize,
-    value: i32,
+    value: i64,
 }
 
 impl Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "[{}]: {} ", self.og_order, self.value)
     }
-}
-
-fn add_one_mod(index: usize, len: usize) -> usize {
-    (index + 1) % len
-}
-
-fn sub_one_mod(index: usize, len: usize) -> usize {
-    (index + len - 1) % len
 }
 
 fn add_mod(index: usize, add: usize, len: usize) -> usize {
@@ -158,13 +286,23 @@ mod tests {
     }
 
     #[test]
+    fn part_2_works() {
+        assert_eq!(1623178306, part_2(&BASIC_INPUT_DAY_20));
+    }
+
+    #[test]
+    fn part_2_smarter_works() {
+        assert_eq!(1623178306, part_2_smarter(&BASIC_INPUT_DAY_20));
+    }
+
+    #[test]
     fn wrap_helpers() {
         let len = 7;
 
-        assert_eq!(5, add_one_mod(4, len));
-        assert_eq!(0, add_one_mod(6, len));
+        assert_eq!(5, add_mod(4, 1, len));
+        assert_eq!(0, add_mod(6, 1, len));
 
-        assert_eq!(4, sub_one_mod(5, len));
-        assert_eq!(6, sub_one_mod(0, len));
+        assert_eq!(4, sub_mod(5, 1, len));
+        assert_eq!(6, sub_mod(0, 1, len));
     }
 }
